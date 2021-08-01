f715059cdfc1fa83e35b27a4d90002d64df1f957
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2021-08-01 16:28:11 +0700

    extract kanji from api

--- kanjigen-be/s2_networkx/dataset/s4_kanjis_generate.py
+++ kanjigen-be/s2_networkx/dataset/s4_kanjis_generate.py
@@ -8,24 +8,37 @@ import requests
 url = "https://kanjiapi.dev/v1/kanji/"
 
 
-with open("s7_edges_kanji_radical.csv") as f:
-    rows = csv.reader(f, delimiter=",")
+with open("s7_edges_kanji_radical.csv") as fin:
+    rows = csv.reader(fin, delimiter=",")
     next(rows)
 
     results = []
-    for row in rows:
-        kanji, radicals = row
-        resp = requests.get(url + kanji).json()
-        element = {
-            "kanji": kanji,
-            "meanings": ":".join(resp["meanings"]),
-        }
+    i = 0
 
-        results += [element]
-        print(len(results))
+    for j in range(3528):
+        next(rows)
+        i += 1
 
-with open("s5_kanjis_output.csv", mode="w") as f:
-    fieldnames = ["kanji", "meanings"]
-    writer = csv.DictWriter(f, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(results)
+    # for row in rows:
+    #     kanji, radicals = row
+    #     print(row)
+    #     assert 1 == 0
+
+    with open("s5_kanjis_output.csv", mode="a") as fout:
+        for row in rows:
+            kanji, radicals = row
+            resp = requests.get(url + kanji).json()
+            element = {
+                "kanji": kanji,
+                "meanings": ":".join(resp["meanings"]),
+            }
+
+            results += [element]
+
+            fout.write(element["kanji"])
+            fout.write(",")
+            fout.write(element["meanings"])
+            fout.write("\n")
+
+            i += 1
+            print(i)
